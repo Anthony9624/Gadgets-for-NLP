f72b503b42c49f17e37f07ca2b6b568794bc258c
Anthony_Yu <42222043+Anthony9624@users.noreply.github.com>
2020-07-17 19:50:26 +0800

    Update comput_CRF.py

--- comput_CRF.py
+++ comput_CRF.py
@@ -4,15 +4,3 @@ author:Anthony_Yu
  compute PRF
 '''
 
-from sklearn.metrics import classification_report as cfr
-import os
-w_true = []#引入数值
-fileread = os.listdir('E:/ok/new54.txt')#读文件
-for line in fileread.readlines():#依次遍历
-    a = w_true.append(line.strip())#删空行
-w_pred = []#引入数值
-fileread = os.listdir('E:/ok/t59.txt')
-for line in fileread.readlines():
-    b= w_pred.append(line.strip())
-
-print(cfr(a, b))#使用引用的模块进行计算
