a2b7532202b08b78ed390ab89f53ab127a44c3ed
Anthony_Yu <42222043+ant19960317@users.noreply.github.com>
2020-07-17 17:01:36 +0800

    Add files via upload
    add knowledge(Add word embeddings to another word embeddings)

---
+++ add.py
@@ -0,0 +1,38 @@
+'''
+Created on 2020年5月29日
+@author: Anthony_Yu
+Add word embeddings to word embeddings
+'''
+
+import io
+embtxt = "F:/train.txt"
+with open(embtxt,"r+") as f:
+ content = f.read()
+f.seek(0,0)
+text = "'PAD'\t"
+for i in range(300):
+    text += "0\t"
+    text = text[0:-1]
+f.write(text + '\n' + content)
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = "你好，我的名字叫"
+# b = "于博"
+# c = a+b
+# print(c)
+
