bdcc4657057b10390713bb14c189b1c6639ac590
Anthony_Yu <42222043+Anthony9624@users.noreply.github.com>
2020-07-20 18:09:26 +0800

    Update compare.py

--- character_string_compare/compare.py
+++ character_string_compare/compare.py
@@ -17,20 +17,20 @@ def blue_color(text):
     return u'\033[1;32;40m%s\033[0m' % text
 
 
-def compare(source, target):
-    _, mappingss = longest_common_subsequence(source, target)
-    source_idxs = set([i for i, j in mappingss])
-    target_idxs = set([j for i, j in mappingss])
-    colored_source, colored_target = u'', u''
-    for i, j in enumerate(source):
-        if i in source_idxs:
-            colored_source += blue_color(j)
+def String_compare(origin, objective):
+    _, mappings = longest_common_subsequence(origin, objective)
+    source_idxs = set([i for i, j in mappings])
+    target_idxs = set([j for i, j in mappings])
+    colored_origin, colored_target = u'', u''
+    for i, j in enumerate(origin):
+        if i in origin_idxs:
+            colored_origin += blue_color(j)
         else:
-            colored_source += red_color(j)
-    for i, j in enumerate(target):
-        if i in target_idxs:
-            colored_target += blue_color(j)
+            colored_origin += red_color(j)
+    for i, j in enumerate(objective):
+        if i in objective_idxs:
+            colored_objective += blue_color(j)
         else:
-            colored_target += red_color(j)
-    print(colored_source)
-    print(colored_target)
+            colored_objective += red_color(j)
+    print(colored_origin)
+    print(colored_objective)
